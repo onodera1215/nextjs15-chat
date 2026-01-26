ec114e282d09aac969de0b7c305fb5b568751ec3
onodera <onodera@h-company.co.jp>
2026-01-26 18:16:29 +0900

    ルーム作成通知用サブスクリプション実装

--- backend/src/room/room.resolver.ts
+++ backend/src/room/room.resolver.ts
@@ -1,10 +1,13 @@
-import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query, Subscription } from '@nestjs/graphql';
 import { RoomNode } from './gql-model/room.model';
 import { CreateRoomInput } from './gql-model/room.input';
 import { CreateRoomUsecase } from './usecase/create-room.usecase';
 import { GetRoomsUsecase } from './usecase/get-rooms.usecase';
 import { SearchRoomOptionInput } from './gql-model/search-room-option.input';
 import { GetRoomUsecase } from './usecase/get-room.usecase';
+import { Inject } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { Public } from 'src/auth/public.decorator';
 
 @Resolver()
 export class RoomResolver {
@@ -12,11 +15,16 @@ export class RoomResolver {
     private readonly createRoomUsecase: CreateRoomUsecase,
     private readonly getRoomsUsecase: GetRoomsUsecase,
     private readonly getRoomUsecase: GetRoomUsecase,
+
+    @Inject('GqlPubSub')
+    private readonly gqlPubSub: PubSub,
   ) {}
 
   @Mutation(() => RoomNode, { description: 'ルーム新規作成' })
   async createRoom(@Args('input') input: CreateRoomInput): Promise<RoomNode> {
-    return await this.createRoomUsecase.execute(input);
+    const room = await this.createRoomUsecase.execute(input);
+    await this.gqlPubSub.publish('roomCreated', { roomCreated: room });
+    return room;
   }
 
   @Query(() => [RoomNode], { description: 'ルーム一覧取得' })
@@ -31,5 +39,10 @@ export class RoomResolver {
     @Args('id', { type: () => String }) id: string,
   ): Promise<RoomNode | null> {
     return await this.getRoomUsecase.execute(id);
+  }
+
+  @Subscription(() => RoomNode)
+  roomCreated() {
+    return this.gqlPubSub.asyncIterableIterator<RoomNode>('roomCreated');
   }
 }
